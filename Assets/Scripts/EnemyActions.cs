8239295a75a32036c8cdc6f84dc1cac22ce0c3c4
Hugo Aguirre <hugo.agmtz@gmail.com>
2022-01-21 21:20:52 +0000

    Enemies move towards mecha

--- Assets/Scripts/EnemyActions.cs
+++ Assets/Scripts/EnemyActions.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class EnemyActions : MonoBehaviour
 {
+    public int speed = 1;
     public int health = 120;
+
+    GameObject mecha;
+
+    void Start()
+    {
+         mecha = GameObject.Find("Mecha");
+    }
+
+    void Update()
+    {
+        MoveTowardsMecha();
+    }
+
+    void MoveTowardsMecha()
+    {
+        // Move
+        transform.position = Vector3.MoveTowards(transform.position, mecha.transform.position, speed * 0.01f);
+
+        // Rotate
+        Vector3 direction = mecha.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
 
     public void Damage(int damage) {
         health -= damage;
