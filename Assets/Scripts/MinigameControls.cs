faae08dae77fe59fa799dc0818805d7f45357627
Jesus Lopez <jesus.loma761@gmail.com>
2022-01-23 01:54:10 -0600

    Single jump with Space key

--- Assets/Scripts/MinigameControls.cs
+++ Assets/Scripts/MinigameControls.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class MinigameControls : MonoBehaviour
 {
     public GameObject player;
-    public float moveSpeed = 5;
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 50.0f;
 
     private Rigidbody2D playerBody;
+    private Collider2D playerCollider;
     private float horizontalMovement = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Rigid body from player object
+        // Rigid body and collider from player object
         playerBody = player.GetComponent<Rigidbody2D>();
+        playerCollider = player.GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,19 @@ public class MinigameControls : MonoBehaviour
     {
         // Move factor from -1 to 1 for left to right
         horizontalMovement = Input.GetAxis("Horizontal");
+
+        ContactPoint2D[] contacts = new ContactPoint2D[10];
+        int contactCount = playerCollider.GetContacts(contacts);
+        float verticalNormalSum = 0.0f;
+        for(int i = 0; i < contactCount; i++){
+            // Debug.LogError(contacts[i].normal);
+            verticalNormalSum += Mathf.Abs(contacts[i].normal.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && verticalNormalSum > 0.0f)
+        {
+            playerBody.AddForce(new Vector2(0.0f,jumpForce),ForceMode2D.Impulse);
+        }
     }
 
     void FixedUpdate()
