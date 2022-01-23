ba961dcdf9b99d52aa3ce2f676c8c1ea20ef85cf
Jesus Lopez <jesus.loma761@gmail.com>
2022-01-23 02:35:14 -0600

    Global handlers to avoid object creation

--- Assets/Scripts/MinigameControls.cs
+++ Assets/Scripts/MinigameControls.cs
@@ -11,6 +11,8 @@ public class MinigameControls : MonoBehaviour
     private Rigidbody2D playerBody;
     private Collider2D playerCollider;
     private float horizontalMovement = 0.0f;
+    private ContactPoint2D[] contacts = new ContactPoint2D[10];
+    private Vector2 velocityHandler;
 
     // Start is called before the first frame update
     void Start()
@@ -25,15 +27,14 @@ public class MinigameControls : MonoBehaviour
     {
         // Move factor from -1 to 1 for left to right
         horizontalMovement = Input.GetAxis("Horizontal");
-
-        ContactPoint2D[] contacts = new ContactPoint2D[10];
+        
+        // Jump is enabled only when is touching floor
         int contactCount = playerCollider.GetContacts(contacts);
         float verticalNormalSum = 0.0f;
-        for(int i = 0; i < contactCount; i++){
-            // Debug.LogError(contacts[i].normal);
+        for(int i = 0; i < contactCount; i++)
+        {
             verticalNormalSum += Mathf.Abs(contacts[i].normal.y);
         }
-
         if (Input.GetKeyDown(KeyCode.Space) && verticalNormalSum > 0.0f)
         {
             playerBody.AddForce(new Vector2(0.0f,jumpForce),ForceMode2D.Impulse);
@@ -43,6 +44,8 @@ public class MinigameControls : MonoBehaviour
     void FixedUpdate()
     {
         // Move Horizontal
-        playerBody.velocity = new Vector2(horizontalMovement * moveSpeed, playerBody.velocity.y);
+        velocityHandler.x = horizontalMovement * moveSpeed;
+        velocityHandler.y = playerBody.velocity.y;
+        playerBody.velocity = velocityHandler;
     }
 }
