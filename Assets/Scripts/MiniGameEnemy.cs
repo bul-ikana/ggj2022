1f803c1919a8a6a212ecf939334165113abb3cd3
Jesus Lopez <jesus.loma761@gmail.com>
2022-01-24 01:25:55 -0600

    Prefab of all elements for minigame

--- Assets/Scripts/MiniGameEnemy.cs
+++ Assets/Scripts/MiniGameEnemy.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class MiniGameEnemy : MonoBehaviour
 {
     private GameObject player;
+    private Rigidbody2D rb;
+
+    private float impulseForce = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(-impulseForce,0.0f),ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
@@ -21,7 +26,8 @@ public class MiniGameEnemy : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == player) {
-            Application.LoadLevel(Application.loadedLevel);
+            rb.AddForce(new Vector2(2*impulseForce,0.0f),ForceMode2D.Impulse);
+            // Application.LoadLevel(Application.loadedLevel);
         }
     }
 }
