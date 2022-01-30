dfb19deabe490fae1d3010378f102c04e0b66683
Homeroe <enape_c@hotmail.com>
2022-01-30 01:31:16 -0600

    Added tileset

--- Assets/Scripts/ChangeSceneScript.cs
+++ Assets/Scripts/ChangeSceneScript.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeSceneScript : MonoBehaviour {
-	public GameObject ScreenTransition;
-	public string screenToChange;
+  public GameObject ScreenTransition;
+  public string screenToChange;
 
   private GameManagerScript gameManager;
 
@@ -13,19 +13,25 @@ public class ChangeSceneScript : MonoBehaviour {
   }
 
   public void changeScene(string sceneToLoad) {
-		// Create a transition, who will call setView at an animation event
-		GameObject transition = Instantiate(ScreenTransition, this.transform.position, this.transform.rotation);
-		DontDestroyOnLoad(transition);
-		transition.GetComponentInChildren<ChangeSceneScript>().screenToChange = sceneToLoad;
+    // Create a transition, who will call setView at an animation event
+    GameObject transition = Instantiate(ScreenTransition, this.transform.position, this.transform.rotation);
+    DontDestroyOnLoad(transition);
+    transition.GetComponentInChildren<ChangeSceneScript>().screenToChange = sceneToLoad;
   }
 
-	public void CallGmChangescene(){
-		gameManager.ChangeView(screenToChange);
-	}
+  public void CallGmChangescene(){
+    gameManager.ChangeView(screenToChange);
+  }
 
-	public void KillTransition(){
-		Destroy(this);
-	}
+  public void KillTransition(){
+    Destroy(transform.parent.gameObject);
+  }
+
+  public void GameoverRetry(string sceneToLoad){
+    gameManager.mechaHealth = gameManager.mechaMaxHealth;
+    gameManager.SetMechaPosition(new Vector3(0,0,0));
+    changeScene(sceneToLoad);
+  }
 
   public void CloseMenuWindow() {
     gameManager.CloseMenuWindow();
