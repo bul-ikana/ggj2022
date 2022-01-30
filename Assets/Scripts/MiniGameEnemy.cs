1853f48086ab0f0c8c07ef948fd78c4fd9c426f2
Jesus Lopez <jesus.loma761@gmail.com>
2022-01-29 19:30:19 -0600

    Enemies start moving on touching floor

--- Assets/Scripts/MiniGameEnemy.cs
+++ Assets/Scripts/MiniGameEnemy.cs
@@ -6,19 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class MiniGameEnemy : MonoBehaviour
 {
-    public float impulseForce = 5.0f;
+    public float speedForce = 5.0f;
 
     private Rigidbody2D rb;
     private int direction = -1;
 
     private GameManagerScript gameManager;
+    private bool moveInit = false;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
         rb = gameObject.GetComponent<Rigidbody2D>();
-        rb.AddForce(new Vector2(direction*impulseForce,0.0f),ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
@@ -29,16 +29,17 @@ public class MiniGameEnemy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        //  Only for debugging
         if (collision.tag == "Player") 
         {
             gameManager.ChangeView(gameManager.getCurrentScene());
         }
-        else{
-			if(collision.tag == "MinigameEnemyReverser"){
-                direction = -direction;
-                rb.AddForce(new Vector2(2*direction*impulseForce,0.0f),ForceMode2D.Impulse);
-            }
+        else if(collision.tag == "MinigameEnemyReverser"){
+            direction = -direction;
+            rb.AddForce(new Vector2(2*direction*speedForce,0.0f),ForceMode2D.Impulse);
         }
+        else if(Mathf.Abs(rb.velocity.x) < Mathf.Abs(speedForce) && !moveInit){
+            moveInit = true;
+            rb.AddForce(new Vector2(direction*speedForce,0.0f),ForceMode2D.Impulse);
+        }        
     }
 }
